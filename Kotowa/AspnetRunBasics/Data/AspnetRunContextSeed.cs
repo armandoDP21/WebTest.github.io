f0e6671c99efc4c3d0088d0a35ee6552b7bf67fd
ArmandoDP21 <armando.diaz.puga@outlook.com>
2020-06-05 16:21:31 +0200

    Carga inicial
    Carga1

---
+++ Kotowa/AspnetRunBasics/Data/AspnetRunContextSeed.cs
@@ -0,0 +1,133 @@
+﻿using AspnetRunBasics.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics.Data
+{
+    public class AspnetRunContextSeed
+    {
+        public static async Task SeedAsync(AspnetRunContext aspnetrunContext, ILoggerFactory loggerFactory, int? retry = 0)
+        {
+            int retryForAvailability = retry.Value;
+
+            try
+            {
+                // TODO: Only run this if using a real database
+                // aspnetrunContext.Database.Migrate();
+                // aspnetrunContext.Database.EnsureCreated();
+
+                if (!aspnetrunContext.Categories.Any())
+                {
+                    aspnetrunContext.Categories.AddRange(GetPreconfiguredCategories());
+                    await aspnetrunContext.SaveChangesAsync();
+                }
+
+                if (!aspnetrunContext.Products.Any())
+                {
+                    aspnetrunContext.Products.AddRange(GetPreconfiguredProducts());
+                    await aspnetrunContext.SaveChangesAsync();
+                }
+            }
+            catch (Exception exception)
+            {
+                if (retryForAvailability < 10)
+                {
+                    retryForAvailability++;
+                    var log = loggerFactory.CreateLogger<AspnetRunContextSeed>();
+                    log.LogError(exception.Message);
+                    await SeedAsync(aspnetrunContext, loggerFactory, retryForAvailability);
+                }
+                throw;
+            }
+        }
+
+        private static IEnumerable<Category> GetPreconfiguredCategories()
+        {
+            return new List<Category>()
+            {
+                new Category()
+                {
+                    Name = "Tri pack",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat.",
+                    ImageName = "one"
+                },
+                new Category()
+                {
+                    Name = "Bio",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat.",
+                    ImageName = "two"
+                },
+                new Category()
+                {
+                    Name = "Tradicional",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat.",
+                    ImageName = "tree"
+                }
+            };
+        }
+
+        private static IEnumerable<Product> GetPreconfiguredProducts()
+        {
+            return new List<Product>()
+            {
+                new Product()
+                {
+                    Name = "Tri pack ",
+                    Summary = "Caja de 3 Cafés Geisha Proceso Natural de 2oz .",                    
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "Cafe01.png",
+                    Price = 25.00M,
+                    CategoryId = 1
+                },
+                new Product()
+                {
+                    Name = "Tri pack",
+                    Summary = "Caja de 3 Cafés Geisha Proceso Natural de 2oz.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "Cafe02.webp",
+                    Price = 25.00M,
+                    CategoryId = 1
+                },
+                new Product()
+                {
+                    Name = "Bio",
+                    Summary = "Descafeinado 8oz Grano.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "Cafe03.png",
+                    Price = 7.85M,
+                    CategoryId = 2
+                },
+                new Product()
+                {
+                    Name = "Bio",
+                    Summary = "Descafeinado 8oz Molido.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "Cafe04.webp",
+                    Price = 7.85M,
+                    CategoryId = 2
+                },
+                new Product()
+                {
+                    Name = "Tradicional",
+                    Summary = "Duncan Orgánico 12oz Grano.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "Cafe05.webp",
+                    Price = 10.75M,
+                    CategoryId = 3
+                },
+                new Product()
+                {
+                    Name = "Tradicional",
+                    Summary = "Duncan Orgánico Proceso Natural 12oz Grano.",
+                    Description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.",
+                    ImageFile = "Cafe06.webp",
+                    Price = 10.75M,
+                    CategoryId = 3
+                }
+            };
+        }
+    }
+}
